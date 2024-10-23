f9f8e40908c972cc8fec58ea81ddf26e401d4a07
Angel Gallego <angelgb1990@gmail.com>
2024-10-23 20:00:03 +0000

    proyecto base finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
+import "/src/styles/index.css"
 import React from "react";
+import Contador from "./Contador";
+import root from "..";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const Iniciar = () => {
+		let count = 0
+		setInterval(function () {
+			let countDecmil = Math.floor((count / 10000) % 10);
+			let countMil = Math.floor((count / 1000) % 10);
+			let countCen = Math.floor((count / 100) % 10);
+			let countDec = Math.floor((count / 10) % 10);
+			let countUni = Math.floor((count / 1) % 10);
+			root.render(
+				<Contador Uni={countUni} Dec={countDec} Cen={countCen} Mil={countMil} Decmil={countDecmil} />
+			);
+	
+			count++;
+		}, 1000);
+	}
+	
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-white pt-4">Bienvenido a el contador</h1>
+			<button onClick={() => {
+				Iniciar()
+			}} type="button" className="btn btn-light">Iniciar Contador</button>
+			
 		</div>
 	);
+	
 };
 
 export default Home;
+// uni={countUni} dec={countDec} cen={countCen} mil={countMil} decmil={countDecmil} 
