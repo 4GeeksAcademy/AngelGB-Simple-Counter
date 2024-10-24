2fd17eefafe3c5ca7d9de60a70c79a5988c8611f
Angel Gallego <angelgb1990@gmail.com>
2024-10-24 20:36:57 +0000

    creado botones y imput

--- src/js/component/Contador.jsx
+++ src/js/component/Contador.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import "/src/styles/index.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 
-function Contador({ Uni, Dec, Cen, Mil, Decmil}) {
- 
+function Contador({ Uni, Dec, Cen, Mil, Decmil }) {
+
     return (
         <div className="contador container col-3 d-flex pt-5">
-                    <div><h1>Icono</h1></div>
-                    <div><h1>{Decmil}</h1></div>
-                    <div><h1>{Mil}</h1></div>
-                    <div><h1>{Cen}</h1></div>
-                    <div><h1>{Dec}</h1></div>
-                    <div><h1>{Uni}</h1></div>
-                </div>
-            );
-        }        
-        export default Contador;
+            <div><h1><FontAwesomeIcon icon={faClock} spin /></h1></div>
+            <div><h1>{Decmil}</h1></div>
+            <div><h1>{Mil}</h1></div>
+            <div><h1>{Cen}</h1></div>
+            <div><h1>{Dec}</h1></div>
+            <div><h1>{Uni}</h1></div>
+        </div>
+        
+    );
+}
+export default Contador;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,38 +1,153 @@
 import "/src/styles/index.css"
+import { useRef } from "react";
 import React from "react";
 import Contador from "./Contador";
 import root from "..";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause, faBackward, faRotateRight } from "@fortawesome/free-solid-svg-icons";
 
-
-//create your first component
 const Home = () => {
-	const Iniciar = () => {
-		let count = 0
-		setInterval(function () {
+	const imputRef = useRef(0);
+	let countContrareloj = 0
+	let count = 0
+	let contadoriniciado = 0
+	let iniciado = true
+	const Pedirtiempo = () => {
+		root.render(
+			<div className="container text-center text-white pt-5">
+				<h1>Por favor ingresa desde que segundo quieres que empiece a retroceder</h1>
+				<div className="mb-3 row justify-content-center pt-4">
+					<div className="col-auto text-center">
+						<input type="number" ref={imputRef} className=" col-auto form-control" placeholder="Numero en segundos..." />
+					</div>
+
+					<div className="col-auto text-center">
+						<button onClick={() => { Guardado() }} type="button" className="col btn btn-light">Iniciar Contrareloj</button>
+					</div>
+
+				</div>
+			</div>
+		)
+
+	}
+	const Guardado = () => {
+		const imputValue = imputRef.current.value;
+		countContrareloj = imputValue
+		Iniciarcontrareloj()
+
+	}
+	const Iniciarcontrareloj = () => {
+
+		contadoriniciado = setInterval(function () {
+			let countDecmil2 = Math.floor((countContrareloj / 10000) % 10);
+			let countMil2 = Math.floor((countContrareloj / 1000) % 10);
+			let countCen2 = Math.floor((countContrareloj / 100) % 10);
+			let countDec2 = Math.floor((countContrareloj / 10) % 10);
+			let countUni2 = Math.floor((countContrareloj / 1) % 10);
+			root.render(
+				<div>
+					<Contador Uni={countUni2} Dec={countDec2} Cen={countCen2} Mil={countMil2} Decmil={countDecmil2} />
+					<div className="container text-center">
+						<div className="btn-group mt-4" role="group" aria-label="Basic outlined example">
+							<button onClick={() => { Pausacontador() }} type="button" className="btn btn-light"><FontAwesomeIcon icon={faPause} /></button>
+							<button onClick={() => { Reanudarcontador() }} type="button" className="btn btn-light"><FontAwesomeIcon icon={faPlay} /></button>
+
+						</div>
+						<div className="container text-center">
+							<button onClick={() => { Volver() }} type="button" className=" mt-4 btn btn-light"><FontAwesomeIcon icon={faBackward} /> Volver a pagina principal</button>
+						</div>
+					</div>
+
+				</div>
+			);
+
+			countContrareloj--;
+		}, 1000)
+		const Pausacontador = () => {
+			if (iniciado == true) {
+				clearInterval(contadoriniciado)
+				iniciado = false
+			}
+			;
+		};
+		const Reanudarcontador = () => {
+			if (iniciado == false) {
+				Iniciarcontrareloj()
+				iniciado = true
+			}
+		};
+	}
+	const Iniciarcontador = () => {
+		contadoriniciado = setInterval(function () {
 			let countDecmil = Math.floor((count / 10000) % 10);
 			let countMil = Math.floor((count / 1000) % 10);
 			let countCen = Math.floor((count / 100) % 10);
 			let countDec = Math.floor((count / 10) % 10);
 			let countUni = Math.floor((count / 1) % 10);
 			root.render(
-				<Contador Uni={countUni} Dec={countDec} Cen={countCen} Mil={countMil} Decmil={countDecmil} />
+				<div>
+					<Contador Uni={countUni} Dec={countDec} Cen={countCen} Mil={countMil} Decmil={countDecmil} />
+					<div className="container text-center">
+						<div className="btn-group mt-4" role="group" aria-label="Basic outlined example">
+							<button onClick={() => { Pausacontador() }} type="button" className="btn btn-light"><FontAwesomeIcon icon={faPause} /></button>
+							<button onClick={() => { Reanudarcontador() }} type="button" className="btn btn-light"><FontAwesomeIcon icon={faPlay} /></button>
+							<button onClick={() => { Reiniciar() }} type="button" className="btn btn-light"><FontAwesomeIcon icon={faRotateRight} /></button>
+						</div>
+						<div className="container text-center">
+							<button onClick={() => { Volver() }} type="button" className=" mt-4 btn btn-light"><FontAwesomeIcon icon={faBackward} /> Volver a pagina principal</button>
+						</div>
+					</div>
+
+				</div>
 			);
-	
+
 			count++;
-		}, 1000);
+		}, 1000)
+
+		const Pausacontador = () => {
+			if (iniciado == true) {
+				clearInterval(contadoriniciado)
+				iniciado = false
+			}
+			;
+		};
+		const Reanudarcontador = () => {
+			if (iniciado == false) {
+				Iniciarcontador()
+				iniciado = true
+			}
+		};
+		const Reiniciar = () => {
+			clearInterval(contadoriniciado)
+			count = 0;
+			iniciado = true
+			Iniciarcontador()
+		};
 	}
-	
+	const Volver = () => {
+		clearInterval(contadoriniciado)
+		iniciado = true
+		root.render(<Home />)
+	};
 	return (
 		<div className="text-center">
-			<h1 className="text-white pt-4">Bienvenido a el contador</h1>
-			<button onClick={() => {
-				Iniciar()
-			}} type="button" className="btn btn-light">Iniciar Contador</button>
-			
+			<h1 className="text-white pt-4">Bienvenido a la web</h1>
+			<h3 className="text-white">Por favor elija entre las opciones.</h3>
+			<div className="mt-4">
+				<div className="btn-group mt-4" role="group" aria-label="Basic outlined example">
+					<button onClick={() => {
+						Iniciarcontador()
+					}} type="button" className="btn btn-light">Iniciar Contador</button>
+					<button onClick={() => {
+						Pedirtiempo()
+					}} type="button" className="btn btn-light">Iniciar Contrareloj</button>
+				</div>
+			</div>
+
+
 		</div>
-	);
-	
+	)
+
 };
 
 export default Home;
-// uni={countUni} dec={countDec} cen={countCen} mil={countMil} decmil={countDecmil} 
